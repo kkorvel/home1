da7590e985c8a0dfc2c254dea1211887b4da2b71
Koit Kõrvel <kkorvel@itcollege.ee>
2016-09-13 02:10:13 +0300

    Java

--- src/Balls.java
+++ src/Balls.java
@@ -2,13 +2,35 @@
 public class Balls {
 
    enum Color {green, red};
-   
+
    public static void main (String[] param) {
       // for debugging
    }
-   
+
    public static void reorder (Color[] balls) {
-      // TODO!!! Your program here
+      int punasepallilugeja = 0;
+
+      // Kui palju punaseid palle seal on?
+      for (int i = 0; i < balls.length; i++) {
+         if (balls[i] == Color.red) {
+            punasepallilugeja++;
+         }
+      }
+
+
+      // Kui kõik pallid on rohelised või punased, siis tagasta need?
+      if (punasepallilugeja == balls.length) {
+         return;
+      } else if (punasepallilugeja == 0) {
+         return;
+      }
+
+      // Asendab pallid õigete värvide ja õige järjekorraga!
+      for (int i = 0; i < punasepallilugeja; i++) {
+         balls[i] = Color.red;
+      }
+      for (int i = punasepallilugeja; i < balls.length; i++) {
+         balls[i] = Color.green;
+      }
    }
 }
-
